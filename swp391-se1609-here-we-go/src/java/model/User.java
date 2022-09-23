1775ef0c4b53bc0279825bc8e36d29860c6d32a4
LongIniesta <104144643+LongIniesta@users.noreply.github.com>
2022-09-23 07:31:24 +0700

    ChangeDTB

---
+++ swp391-se1609-here-we-go/src/java/model/User.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author baolo
+ */
+public class User {
+    private long userId;
+    private String name;
+    private String phone;
+    private String avtLink;
+    private int roleId;
+    private String password;
+
+    public User() {
+    }
+
+    public User(long userId, String name, String phone, String avtLink, int roleId, String password) {
+        this.userId = userId;
+        this.name = name;
+        this.phone = phone;
+        this.avtLink = avtLink;
+        this.roleId = roleId;
+        this.password = password;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAvtLink() {
+        return avtLink;
+    }
+
+    public void setAvtLink(String avtLink) {
+        this.avtLink = avtLink;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
